57c065e75b563550c3ff41b694b6bc8dcdc570bf
James Messinger <james.messinger@gmail.com>
2020-03-07 11:48:35 -0600

    Updated tests to work in browsers as well as Node.  Added checks for isomorphic behavior

--- test/specs/api.spec.js
+++ test/specs/api.spec.js
@@ -1,36 +1,66 @@
 "use strict";
 
-const projectExportName = require("../../");
-const { expect } = require("chai");
+const { projectExportName } = require("../../");
+const { assert, expect } = require("chai");
+const { host } = require("@jsdevtools/host-environment");
+
+const location = host.node ? process.cwd() : window.location.href;
 
 describe("projectExportName() API", () => {
 
   it("should work without any arguments", () => {
     let result = projectExportName();
-    expect(result).to.equal("Hello, world.");
+    expect(result).to.equal(`Hello, world from ${location}.`);
   });
 
   it("should accept a custom greeting", () => {
     let result = projectExportName({ greeting: "Hi there" });
-    expect(result).to.equal("Hi there, world.");
+    expect(result).to.equal(`Hi there, world from ${location}.`);
   });
 
   it("should accept a custom subject", () => {
     let result = projectExportName({ subject: "Michael" });
-    expect(result).to.equal("Hello, Michael.");
+    expect(result).to.equal(`Hello, Michael from ${location}.`);
+  });
+
+  it("should accept a custom location", () => {
+    let result = projectExportName({ location: "New York City" });
+    expect(result).to.equal("Hello, world from New York City.");
   });
 
   it("should accept a custom greeting and subject", () => {
     let result = projectExportName({ greeting: "Yo", subject: "man" });
-    expect(result).to.equal("Yo, man.");
+    expect(result).to.equal(`Yo, man from ${location}.`);
+  });
+
+  it("should accept a custom greeting, subject, and location", () => {
+    let result = projectExportName({ greeting: "Yo", subject: "man", location: "outer space" });
+    expect(result).to.equal("Yo, man from outer space.");
+  });
+
+  it("should set the location based on the runtime host", () => {
+    let result = projectExportName();
+
+    if (host.browser) {
+      expect(result).to.match(/^Hello, world from http:\/\/localhost/);
+    }
+    else if (host.os.windows) {
+      expect(result).to.match(/^Hello, world from [A-Z]:\\/);
+    }
+    else {
+      expect(result).to.match(/^Hello, world from \//);
+    }
   });
 
   it('should not allow a greeting of "goodbye"', () => {
-    function sayGoodbye () {
+    try {
       projectExportName({ greeting: "Goodbye" });
+      assert.fail("An error should have been thrown!");
     }
-
-    expect(sayGoodbye).to.throw("Cannot say goodbye");
+    catch (error) {
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal("Cannot say goodbye");
+    }
   });
 
 });
--- test/specs/exports.spec.js
+++ test/specs/exports.spec.js
@@ -3,22 +3,29 @@
 const commonJSExport = require("../../");
 const { default: defaultExport, projectExportName: namedExport } = require("../../");
 const { expect } = require("chai");
+const { host } = require("@jsdevtools/host-environment");
 
 describe("project-package-name package exports", () => {
 
   it("should export the projectExportName() function as the default CommonJS export", () => {
-    expect(commonJSExport).to.be.a("function");
-    expect(commonJSExport.name).to.equal("projectExportName");
+    if (host.node) {
+      expect(commonJSExport).to.be.a("function");
+      expect(commonJSExport.name).to.equal("projectExportName");
+    }
+    else {
+      // Browser tests are only ESM, not CommonJS
+      expect(commonJSExport).to.be.a("Module").with.keys("default", "projectExportName");
+    }
   });
 
   it("should export the projectExportName() function as the default ESM export", () => {
     expect(defaultExport).to.be.a("function");
-    expect(defaultExport).to.equal(commonJSExport);
+    expect(defaultExport.name).to.equal("projectExportName");
   });
 
   it("should export the projectExportName() function as a named export", () => {
     expect(namedExport).to.be.a("function");
-    expect(namedExport).to.equal(commonJSExport);
+    expect(namedExport.name).to.equal("projectExportName");
   });
 
   it("should not export anything else", () => {
